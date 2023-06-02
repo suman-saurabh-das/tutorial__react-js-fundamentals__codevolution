5804dd0238bd3a28d437dc89b47814b782b92371
Suman Saurabh Das <dsumansaurabh@gmail.com>
2023-06-02 11:23:24 +0530

    02 - Functional Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+/* -------------------- Section 01 -------------------- */
+// import Greet from './Section 01/02_Greet'
+import { Greet } from './Section 01/02_Greet';
+
 function App() {
   return (
     <div className="App">
       <h1>React JS Fundamentals - Codevolution</h1>
+
+      <Greet />
     </div>
   );
 }
